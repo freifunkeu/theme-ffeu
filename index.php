8163ddc67a0a5fb215f6177be7ee25f55a83c118
freifunkeu <info@freifunk-euskirchen.de>
2014-04-16 00:54:35 +0200

    css url update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head><title>
 	<?php wp_title(' - ', true, 'right'); ?> <?php bloginfo('name'); ?>
-</title><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet" type="text/css" /><meta name="viewport" content="width = 1000" /><link href="wp-content/themes/Freifunk-Webseite/style.css" rel="stylesheet" type="text/css" /><link rel="icon" href="/favicon.ico" type="image/x-icon" />
+</title><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet" type="text/css" /><meta name="viewport" content="width = 1000" /><link href="<?php echo  get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet" type="text/css" /><link rel="icon" href="/favicon.ico" type="image/x-icon" />
 </head>
 <body>
     <div id="header">
