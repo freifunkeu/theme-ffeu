c562e5aa2f85da24c011b285b122cd0208d7b955
Andreas Krischer <a.krischer@akbyte.com>
2015-11-26 21:14:03 +0100

    Header Bugfix
    `wp_head()` has to be between `<head>` and `</head>`

--- header.php
+++ header.php
@@ -1,8 +1,11 @@
-   <?php wp_head(); ?> <!DOCTYPE HTML>
+<!DOCTYPE HTML>
 <html>
-<head><title>
-	<?php wp_title(' - ', true, 'right'); ?> <?php bloginfo('name'); ?>
-</title><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet" type="text/css" /><meta name="viewport" content="width = 1000" /><link href="<?php echo  get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet" type="text/css" /><link rel="icon" href="<?php echo  get_bloginfo('template_directory'); ?>/images/favicon.ico" type="image/x-icon" />
+<head>
+	<title>
+		<?php wp_title(' - ', true, 'right'); ?> <?php bloginfo('name'); ?>
+	</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet" type="text/css" /><meta name="viewport" content="width = 1000" /><link href="<?php echo  get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet" type="text/css" /><link rel="icon" href="<?php echo  get_bloginfo('template_directory'); ?>/images/favicon.ico" type="image/x-icon" />
+	<?php wp_head(); ?> 
 </head>
 <body>
     <div id="header">
